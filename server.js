3251e005003ead08f3d6cca9b174dfd240cd858e
Joel Margosian <margosij@gmail.com>
2019-12-14 16:26:28 -0500

    stupid thing is scraping correctly now

--- server.js
+++ server.js
@@ -27,22 +27,23 @@ app.get("/scrape", function(req, res) {
     axios.get("https://www.businessinsider.com/").then(function(response) {
       // Then, we load that into cheerio and save it to $ for a shorthand selector
       var $ = cheerio.load(response.data);
-  
+// console.log(response.data)
       // Now, we grab every h2 within an article tag, and do the following:
-      $("article h2").each(function(i, element) {
+      $(".tout-title").each(function(i, element) {
         // Save an empty result object
         var result = {};
-  
         result.title = $(this)
           .children("a")
           .text();
-        result.description = $(this)
-          .children("a")
+        result.summary = $(this).parent()
+          .children(".tout-copy")
           .text();
+          //add in https://www.businessinsider.com/ so the href's completely work
         result.link = $(this)
           .children("a")
           .attr("href");
-        
+// console.log(toutTitle, "title")
+// console.log(result, "results")
         // Create a new Article using the `result` object built from scraping
         db.Article.create(result)
           .then(function(dbArticle) {
